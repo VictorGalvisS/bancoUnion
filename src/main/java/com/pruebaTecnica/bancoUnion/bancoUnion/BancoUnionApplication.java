bb2a8bdd938e6cde57f74ad0a9e17eda81d8b68b
Victor Galvis <victor.galvis@onesystem.com.co>
2023-08-15 17:00:21 -0500

    Initial commit

---
+++ src/main/java/com/pruebaTecnica/bancoUnion/bancoUnion/BancoUnionApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.bancoUnion.bancoUnion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoUnionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoUnionApplication.class, args);
+	}
+
+}
